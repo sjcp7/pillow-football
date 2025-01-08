b6abcf5e75659a3f43ed46e422cbda285a2a77d4
gumball <dariltonmario14@gmail.com>
2025-01-08 12:23:44 +0100

    Add numberline for both x and y axis

--- game.py
+++ game.py
@@ -1,11 +1,22 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 from field import *
 
 #14 inch resolution
 imageHeight = 1080
 imageWidth = 1920
 
-img = Image.new("RGBA", (imageWidth, imageHeight), "grey")
+img = Image.new("RGBA", (imageWidth, imageHeight), "white")
 d = ImageDraw.Draw(img)
-draw_field(d, 1080, (60,60)) 
+# Draw guide lines
+numVetLines = 20
+for i in range(0,imageHeight, imageHeight // numVetLines):
+    d.text((15,i), str(i), "black");
+    d.line((0, i + 4) + (10, i + 4), "black");
+
+numHorLines = 50
+for i in range(0,imageWidth, imageWidth // numHorLines):
+    d.text((i,imageHeight - 30), str(i), "black");
+    d.line((i+5, imageHeight) + (i+5, imageHeight - 15), "black");
+
+draw_field(d)
 img.show()
