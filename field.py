3ae1fde02c6c9668c58b5b0cadd2015c90337aaf
gumball <dariltonmario14@gmail.com>
2025-01-09 15:32:21 +0100

    Draw penalty spots

--- field.py
+++ field.py
@@ -2,7 +2,7 @@ from PIL import ImageDraw
 
 # proportions from the real world are maintained through the drawings
 # they are based on the bigger rectangle that makes the field
-
+print('PIL',PIL.__version__)
 screen_width = 1920
 field_height = 300
 field_offset_x = 200 # the difference between the x value of the upper and lower lines of the field
@@ -14,23 +14,25 @@ right_m = - left_m # slope of the left line of the field
 def draw_field(d: ImageDraw):
     draw_bigger_rectangle(d)
     draw_center(d)
+    draw_penalty_circle(d, 390, field_lower_y - 22 - (field_height / 2), 120, 40)
+    draw_penalty_circle(d, screen_width-390, field_lower_y - 22 - (field_height / 2), 120, 40)
     draw_goals(d)
-    draw_goal_boxes(d)
     draw_penalty_boxes(d)
-
+    draw_goal_boxes(d)
+    d.ellipse((390, field_lower_y - 22 - (field_height / 2)), 10,fill="white")
 def draw_penalty_boxes(d: ImageDraw):
     # left penalty box
     b = field_lower_y - (left_m * field_lower_x);
     x1 = 215
     x2 = x1 + 100
     box_width = 150
-    d.polygon(((x1, x1*left_m + b), (x2, x2*left_m + b),(x2 + box_width, (x2*left_m + b)), (x1 + box_width, (x1*left_m + b))), width=3)
+    d.polygon(((x1, x1*left_m + b), (x2, x2*left_m + b),(x2 + box_width, (x2*left_m + b)), (x1 + box_width, (x1*left_m + b))), width=3, outline="white",fill="green")
 
     # right penalty box
     b = field_lower_y - (right_m * (screen_width - field_lower_x))
     x1 = screen_width - x1
     x2 = x1 - 100
-    d.polygon(((x1, x1*right_m + b), (x2, x2*right_m + b),(x2 - box_width, (x2*right_m + b)), (x1 - box_width, (x1*right_m + b))), width=3)
+    d.polygon(((x1, x1*right_m + b), (x2, x2*right_m + b),(x2 - box_width, (x2*right_m + b)), (x1 - box_width, (x1*right_m + b))), width=3, outline="white",fill="green")
 
 def draw_goal_boxes(d: ImageDraw):
     # left goal box
@@ -38,13 +40,13 @@ def draw_goal_boxes(d: ImageDraw):
     x1 = 237
     x2 = x1 + 55
     box_width = 70
-    d.polygon(((x1, x1*left_m + b), (x2, x2*left_m + b),(x2 + box_width, (x2*left_m + b)), (x1 + box_width, (x1*left_m + b))), width=3)
+    d.polygon(((x1, x1*left_m + b), (x2, x2*left_m + b),(x2 + box_width, (x2*left_m + b)), (x1 + box_width, (x1*left_m + b))), width=3, outline="white",fill="green")
 
     # right goal box
     b = field_lower_y - (right_m * (screen_width - field_lower_x))
     x1 = screen_width - x1
     x2 = x1 - 55
-    d.polygon(((x1, x1*right_m + b), (x2, x2*right_m + b),(x2 - box_width, (x2*right_m + b)), (x1 - box_width, (x1*right_m + b))), width=3)
+    d.polygon(((x1, x1*right_m + b), (x2, x2*right_m + b),(x2 - box_width, (x2*right_m + b)), (x1 - box_width, (x1*right_m + b))), width=3, outline="white",fill="green")
 
 def draw_goals(d: ImageDraw):
     # left goal
@@ -63,6 +65,10 @@ def draw_goals(d: ImageDraw):
     d.polygon(((x1, x1*right_m + b), (x2, x2*right_m + b),(x2, (x2*right_m + b) - goal_height), (x1, (x1*right_m + b) - goal_height)), width=3)
 
     d.polygon(((x1, x1*right_m + b), (x1, (x1*right_m + b) - goal_height), (x1+tw, (x1+tw)*right_m + b - tw*right_m)), width=3);
+
+def draw_penalty_circle(d: ImageDraw, cx: int, cy: int, width: int, height: int):
+    d.ellipse((cx - width / 2, cy - height / 2) + (cx + width / 2, cy + height / 2), outline = "white", width=3)
+    
 def draw_center(d: ImageDraw):
     width = 300
     height = 100
