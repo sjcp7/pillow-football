f6205c3542643b55cefb5fb1b971df7fe93930e4
Samuel Pedro <redsamuel.cruz7@gmail.com>
2025-01-09 19:19:35 +0100

    create frames folder in case it doesn't exist

--- create_animation.sh
+++ create_animation.sh
@@ -5,6 +5,7 @@ if [ "$#" -ne 1 ]; then
 	echo "Usage: create_animation.sh <python binary>"
 	exit
 fi
+mkdir -p frames
 rm frames/*
 $1 game.py
 ffmpeg -f image2 -framerate 12 -i frames/img-%03d.png game.mp4
