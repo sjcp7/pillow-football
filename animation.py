f37328e718e56a4c657b88d7bbd1456a0b5a870e
Samuel Pedro <redsamuel.cruz7@gmail.com>
2025-01-09 19:14:11 +0100

    first game scene

--- animation.py
+++ animation.py
@@ -4,23 +4,53 @@ from field import *
 from ball import *
 from player import *
 
-ball = [(760, 760),(664, 702)]
+ball = [(760, 780),(664, 692)]
 pos = [
-        [(760, 760), (760, 760)],
-        [(646, 648), (664, 702)],
+        # team 1
+        [(760, 820, "black"), (780, 810, "black")], # p1, throw-in player, right winger
+        [(646, 648, "black"), (664, 702, "black")], # p2, receiver of the throw-in, striker
+        [(950, 560, "black"), (950, 590, "black")], # p3, left winger
+        [(532, 575, "black"), (537, 579, "black")], # p4, left defender
+        [(560, 770, "black"), (540, 770, "black")], # p5, right defender
+        [(266, 648, "yellow"), (270, 648, "yellow")], # p6, keeper
+
+        # team 2
+        [(836, 770, "white"), (772, 760, "white")], # p1, left winger
+        [(700, 630, "white"), (664, 638, "white")], # p2, striker
+        [(1026, 540, "white"), (1026, 555, "white")], # p3, right winger
+        [(1212, 660, "white"), (1216, 663, "white")], # p4, right defender
+        [(947, 705, "white"), (950, 702, "white")], # p5, left defender
+        [(1560, 632, "red"), (1555, 632, "red")], # p6, keeper
 ]
 
 dimensions = (1920, 1080)
 
+def draw_guide_lines(d: ImageDraw):
+    imageHeight = dimensions[1]
+    imageWidth = dimensions[0]
+
+    numVetLines = 20
+    for i in range(0,imageHeight, imageHeight // numVetLines):
+        d.text((15,i), str(i), "black");
+        d.line((0, i + 4) + (10, i + 4), "black");
+    
+    numHorLines = 50
+    for i in range(0,imageWidth, imageWidth // numHorLines):
+        d.text((i,imageHeight - 30), str(i), "black");
+        d.line((i+5, imageHeight) + (i+5, imageHeight - 15), "black");
+
+
 def create_image(dimensions: Tuple[int, int]):
     img = Image.new("RGBA", dimensions, "grey")
+    d = ImageDraw.Draw(img)
+    draw_guide_lines(d)
     return img
 
-def draw_positions(d: ImageDraw, ball_pos: Tuple[int, int], players_pos: List[Tuple[int, int]]):
+def draw_positions(d: ImageDraw, ball_pos: Tuple[int, int], players_pos: List[Tuple[int, int, str]]):
     draw_field(d)
     draw_ball(d, ball_pos[0], ball_pos[1])
     for i in range(len(players_pos)):
-        draw_player(d, "black", players_pos[i][0], players_pos[i][1])
+        draw_player(d, players_pos[i][2], players_pos[i][0], players_pos[i][1])
     
 def animate():
     num_frames = 10
@@ -32,7 +62,7 @@ def animate():
                 bpos = (ball[i][0] + j * (ball[i+1][0] - ball[i][0])/num_frames, ball[i][1] + j*(ball[i+1][1] - ball[i][1])/num_frames)
                 step_x = j * (pos[k][i + 1][0] - pos[k][i][0]) / num_frames
                 step_y = j * (pos[k][i + 1][1] - pos[k][i][1]) / num_frames
-                ppos.append((pos[k][i][0] + step_x, pos[k][i][1] + step_y))
+                ppos.append((pos[k][i][0] + step_x, pos[k][i][1] + step_y, pos[k][i][2]))
 
             img = create_image(dimensions)
             d = ImageDraw.Draw(img)
--- player.py
+++ player.py
@@ -2,19 +2,25 @@ from PIL import Image, ImageDraw, ImageFont
 from field import *
 
 def draw_player(d: ImageDraw, color: str, x: int, y: int, scale: float = 0.7):
-    # head
     head_radius = int(10 * scale)
+    torso_height = int(40 * scale)
+    # split the player frame in 5 (roughly) equal parts
+    leg_spacing = (head_radius * 2) // 5
+    leg_height = int(25 * scale)
+
+    # for legacy reasons, we were getting the position of the upper left corner of the player
+    # adjusting (x, y) to point to where the player's feet should be
+    x -= head_radius // 2
+    y -= head_radius * 2 + torso_height + leg_height
+
+    # head
     d.circle((x + head_radius, y + head_radius), head_radius, "blue")
     y += 2 * head_radius
 
     # torso
-    torso_height = int(40 * scale)
     d.rectangle([(x, y), (x + 2 * head_radius, y + torso_height)], color)
     y += torso_height
 
-    # split the player frame in 5 (roughly) equal parts
-    leg_spacing = (head_radius * 2) // 5
-    leg_height = int(25 * scale)
     
     # left leg
     d.line([(x + leg_spacing, y), (x + leg_spacing, y + leg_height)], color, leg_spacing)
