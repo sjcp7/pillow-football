7af661437915027be530158226be2d78716e0434
gumball <dariltonmario14@gmail.com>
2025-01-09 19:11:14 +0100

    Add script to create run program and create animatino

---
+++ create_animation.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: create_animation.sh <python binary>"
+	exit
+fi
+rm frames/*
+$1 game.py
+ffmpeg -f image2 -framerate 12 -i frames/img-%03d.png game.mp4
